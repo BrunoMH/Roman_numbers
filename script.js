51b161f400b37074a9421d72e38d6d52d1b830ae
Bruno_MH <brunomh141a@gmail.com>
2025-03-08 13:02:00 -0600

    feat(script.js| index.html| style.css) logic and functionality files added

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const numberInput = document.getElementById('number-input');
+    const checkBtn = document.getElementById('check-btn');
+    const result = document.getElementById('result');
+
+    checkBtn.addEventListener("click", function () {
+        const decimalNumber = parseInt(numberInput.value.trim(), 10);
+        console.log(decimalNumber);
+        if (isNaN(decimalNumber) || decimalNumber <= 0) {
+            alert('Please enter a valid positive number');
+            return;
+        }
+
+        const Roman = romanConverter(decimalNumber);
+        console.log(Roman);
+        result.textContent = `${decimalNumber} is ${Roman} in the Roman numeral system`;
+        
+    });
+
+    function romanConverter(num) {
+        const romanNumerals = [
+            { value: 1000, numeral: 'M' },
+            { value: 900, numeral: 'CM' },
+            { value: 500, numeral: 'D' },
+            { value: 400, numeral: 'CD' },
+            { value: 100, numeral: 'C' },
+            { value: 90, numeral: 'XC' },
+            { value: 50, numeral: 'L' },
+            { value: 40, numeral: 'XL' },
+            { value: 10, numeral: 'X' },
+            { value: 9, numeral: 'IX' },
+            { value: 5, numeral: 'V' },
+            { value: 4, numeral: 'IV' },
+            { value: 1, numeral: 'I' }
+        ];
+
+        let romanNumeral = '';
+        for (let i = 0; i < romanNumerals.length; i++) {
+            while (num >= romanNumerals[i].value) {
+                romanNumeral += romanNumerals[i].numeral;
+                num -= romanNumerals[i].value;
+            }
+        }
+        console.log(romanNumeral);
+        return romanNumeral;
+    }
+});
